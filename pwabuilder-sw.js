856c43b479271a1f63155cd1d530184219e41edd
WaltemarCarneiro <78711339+WaltemarCarneiro@users.noreply.github.com>
2022-07-22 03:35:42 -0400

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,83 +1,49 @@
-// This is the service worker with the Cache-first network
+const cacheName = 'AppCanapum'
 
-const CACHE = "pwabuilder-precache";
-const precacheFiles = [
-  /* Add an array of files to precache for your app */
-];
-
-self.addEventListener("install", function (event) {
-  console.log("[PWA Builder] Install Event processing");
-
-  console.log("[PWA Builder] Skip waiting on install");
-  self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE).then(function (cache) {
-      console.log("[PWA Builder] Caching pages during install");
-      return cache.addAll(precacheFiles);
-    })
-  );
-});
-
-// Allow sw to control of current page
-self.addEventListener("activate", function (event) {
-  console.log("[PWA Builder] Claiming clients for current page");
-  event.waitUntil(self.clients.claim());
-});
-
-// If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener("fetch", function (event) { 
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fromCache(event.request).then(
-      function (response) {
-        // The response was found in the cache so we responde with it and update the entry
-
-        // This is where we call the server to get the newest version of the
-        // file to use the next time we show view
-        event.waitUntil(
-          fetch(event.request).then(function (response) {
-            return updateCache(event.request, response);
-          })
-        );
-
-        return response;
-      },
-      function () {
-        // The response was not found in the cache so we look for it on the server
-        return fetch(event.request)
-          .then(function (response) {
-            // If request was success, add or update it in the cache
-            event.waitUntil(updateCache(event.request, response.clone()));
-
-            return response;
-          })
-          .catch(function (error) {
-            console.log("[PWA Builder] Network request failed and no cache." + error);
-          });
-      }
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache){
+            cache.addAll([
+                './',
+                './index.html',
+                './manifest.webmanifest',
+                './index.js'
+            ])
+        })
     )
-  );
-});
+    return self.skipWaiting()
+})
 
-function fromCache(request) {
-  // Check to see if you have it in the cache
-  // Return response
-  // If not in the cache, then return
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      if (!matching || matching.status === 404) {
-        return Promise.reject("no-match");
-      }
+self.addEventListener('activate', e =>{
+    self.clients.claim()
+})
 
-      return matching;
-    });
-  });
+self.addEventListener('fetch', async e =>{
+    const req = e.request
+    const url = new URL(req.url)
+
+    if(url.origin === location.origin){
+        e.respondWith(cacheFirst(req))
+    } else{
+        e.respondWith(networkAndCache(req))
+    }
+})
+
+async function cacheFirst(req){
+    const cache = await caches.open(cacheName)
+    const cached = await cache.match(req)
+
+    return cached || fetch(req)
 }
 
-function updateCache(request, response) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.put(request, response);
-  });
+async function networkAndCache(req){
+    const cache = await caches.open(cacheName);
+    try{
+        const refresh = await fetch(req)
+        await cache.put(req, fresh.clone())
+        return refresh
+    } catch(e){
+        const cached = await cache.match(req);
+        return cached
+    }
 }
